aa54865afeed9a7877ec06cb448fffa69594ede3
var1191 <93372164+var1191@users.noreply.github.com>
2023-01-14 14:09:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,16 @@ st.title('Real-Time Prediction and Visualization')
 # Read the dataset
 new_df = pd.read_csv('final_df.csv')
 
+
+#===================================================
 # Linear Regression
 st.markdown('## Linear Regression Model for Prediction of TotalSpent_RM')
+
+df_reg = new_df.copy()
+df_reg = df_reg.drop(columns=['Date', 'Time'])
+
+st.markdown('We use all the variable except Date and Time.')
+st.dataframe(df_reg.head()
 
 
 

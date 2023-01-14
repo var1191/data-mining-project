87918618f07e203f214448868f5d5fac74e368d8
var1191 <93372164+var1191@users.noreply.github.com>
2023-01-14 10:50:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 #import folium
-from streamlit_folium import folium_static
+#from streamlit_folium import folium_static
 
 st.write('Hello World!')
 

17d1269c8fdc77f2ef8a3f563fb46d2a58904ae2
var1191 <93372164+var1191@users.noreply.github.com>
2023-01-14 16:40:34 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import pandas as pd
 import folium
 from streamlit_folium import folium_static
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 st.set_page_config(
     page_title = 'Real-Time Prediction and Visualization',
@@ -28,8 +30,26 @@ df_reg = new_df.copy()
 df_reg = df_reg.drop(columns=['Date', 'Time'])
 st.dataframe(df_reg.head())
 
+# select a column as 'Y'
 choice_reg = st.selectbox('Choose a variable to predict:', df_reg.columns)
 
+# Dummify it
+X = df_reg.loc[:, df_reg.columns != choice_reg] 
+Y = df_reg.loc[:, df_reg.columns == choice_reg] 
+
+le = LabelEncoder()
+dfReg_label_dict = {} # to get the label detail
+
+# if 'Y' is Object, encode it using Label Encoder
+if df_reg[choice_reg].dtype == 'O':
+    dfReg_label_dict[Y.columns] = {}
+    Y = le.fit_transform(Y)
+    dfReg_label_dict[Y.columns].update(dict(zip(le.classes_, range(len(le.classes_)))))
+
+X = pd.get_dummies(data=X)
+
+# Model Construction
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=101)
 
 
 

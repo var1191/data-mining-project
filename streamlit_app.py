f2a253702270f9242ac2bf1205387f7eea21bd3c
var1191 <93372164+var1191@users.noreply.github.com>
2023-01-14 20:27:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,32 +30,11 @@ df_reg = new_df.copy()
 df_reg = df_reg.drop(columns=['Date', 'Time'])
 st.dataframe(df_reg.head())
 
-# make 'TotalSpent_RM' at first
-choice_list = ['TotalSpent_RM']
-for i in range(len(df_reg.columns)):
-    choice_list.append(df_reg.columns[i])
-#choice_list = list(set(choice_list)) # remove duplicate
-
-# select a column as 'Y'
-choice_reg = st.selectbox('Choose a variable to predict:', choice_list)
-print(choice_list)
-
 # Dummify it
-X = df_reg.loc[:, df_reg.columns != choice_reg] 
-Y = df_reg.loc[:, df_reg.columns == choice_reg] 
-
-le = LabelEncoder()
-dfReg_label_dict = {} # to get the label detail
-
-# if 'Y' is Object, encode it using Label Encoder
-if df_reg[choice_reg].dtype == 'O':
-    dfReg_label_dict[choice_reg] = {}
-    Y = le.fit_transform(Y)
-    dfReg_label_dict[choice_reg].update(dict(zip(le.classes_, range(len(le.classes_)))))
+X = df_reg.loc[:, df_reg.columns != 'TotalSpent_RM'] 
+Y = df_reg.loc[:, df_reg.columns == 'TotalSpent_RM'] 
 
 X = pd.get_dummies(data=X)
 
 # Model Construction
-testSize = st.slider('Choose the test size:', min_value= 0.1, max_value= 0.5, step=0.1)
-
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=101)

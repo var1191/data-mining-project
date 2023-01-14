ef18f8e1b1f5a30d2518d7ce040a7822c9aae064
var1191 <93372164+var1191@users.noreply.github.com>
2023-01-14 11:44:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,9 @@ st.text('My First Streamlit!')
 st.header('This is a header.')
 st.subheader('This is a subheader.')
 
-st.markdown('**Hello World**')
-st.markdown('*Hello World*')
+st.markdown('** Hello World **')
+st.markdown('* Hello World *')
+
+st.markdown('# Header in markdown')
 
 

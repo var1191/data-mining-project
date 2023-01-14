998c5a159b102f2b89dc08d0b8ca75cd08702e02
var1191 <93372164+var1191@users.noreply.github.com>
2023-01-14 12:45:53 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,6 @@ from streamlit_folium import folium_static
 st.title('Hello World!')
 st.text('My First Streamlit!')
 
-st.header('This is a header.')
-st.subheader('This is a subheader.')
-
-st.markdown('**Hello World**')
-st.markdown('*Hello World*')
-
 st.markdown('# Header in markdown')
 
 
